2d2977bc8a21bcfdc34389f1c57ca43f71b5e90c
Monsalbo Lucio Alejandro Valerio <78209539+LucioAVM@users.noreply.github.com>
2023-08-02 17:27:33 +0000

    Primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Monsalbo Lucio",
+  title: "Estudiante de Tecnicatura Universitaria en Programacion - UTN",
+  email: "lucioavm8@gmail.com",
+  gitHub: "LucioAVM",
+  instagram: "MonsalboLucio",
+  linkedIn: "www.linkedin.com/in/monsalbolucio",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
